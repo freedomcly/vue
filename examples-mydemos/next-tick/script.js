22b875457361537b9eb7a2e0a5284fa215f7ebf0
cuilingyun <cuilingyun@zhongan.com>
2018-06-20 21:16:10 +0800

    add demo next-tick

--- examples-mydemos/next-tick/script.js
+++ examples-mydemos/next-tick/script.js
@@ -1,4 +1,15 @@
 
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  created () {
+    Vue.nextTick(() => {
+      console.log('nextTick1')
+    })
+    setTimeout(() => {
+      console.log('setTimeout')
+    })
+    Vue.nextTick(() => {
+      console.log('nextTick2')
+    })
+  }
 })
